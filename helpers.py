cdffe563898418faed4a3566a6f500b52842b1e5
Jonasz Karwacki <jonaszakr@gmail.com>
2019-10-17 16:12:46 -0700

    yapf reformat

--- helpers.py
+++ helpers.py
@@ -2,7 +2,6 @@ import os
 import requests
 import time
 
-
 import boto3
 from botocore.exceptions import ClientError
 from dotenv import load_dotenv
@@ -11,6 +10,7 @@ from flask import session
 from app import app
 
 load_dotenv()
+
 
 def transcribe(filename):
     transcribe = boto3.client(
@@ -36,11 +36,16 @@ def transcribe(filename):
     print(status)
     return status
 
+
 def upload_to_s3(filepath, filename):
-    s3 = boto3.client("s3", aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"), aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"), region_name='us-west-1')
+    s3 = boto3.client("s3",
+                      aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+                      aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+                      region_name='us-west-1')
     with open(filepath, "rb") as file:
         try:
-            res = s3.upload_fileobj(file, os.getenv("AWS_BUCKET_NAME"), filename)
+            res = s3.upload_fileobj(file, os.getenv("AWS_BUCKET_NAME"),
+                                    filename)
             return res
         except ClientError as e:
             raise e
@@ -49,16 +54,22 @@ def upload_to_s3(filepath, filename):
 def transcript_exists(transcript_id):
     return transcript_id in session
 
+
 def transcript_by_id(transcript_id):
     if transcript_exists(transcript_id):
         return {
-            "transcript_title": session[transcript_id]["jobName"],
-            "transcript_content": session[transcript_id]["results"]["transcripts"][0]["transcript"]
+            "transcript_title":
+            session[transcript_id]["jobName"],
+            "transcript_content":
+            session[transcript_id]["results"]["transcripts"][0]["transcript"]
         }
-    raise FileNotFoundError(f"The transcription {transcription_id} doesn't exist")
+    raise FileNotFoundError(
+        f"The transcription {transcription_id} doesn't exist")
+
 
 def file_is_valid(filename):
     return True
+
 
 def load_json_from_uri(uri):
     return requests.get(uri).json()
