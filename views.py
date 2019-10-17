80c8b7684a40c04f7f47ff918c9790bb395d0887
Jonasz Karwacki <jonaszakr@gmail.com>
2019-10-17 08:20:26 -0700

    add a route (overloading "/") which handles file upload

--- views.py
+++ views.py
@@ -1,6 +1,26 @@
-from flask import render_template
+import os
+
+from flask import flash, redirect, render_template, request, url_for
+from werkzeug import secure_filename
 from app import app
+
+def file_is_valid(filename):
+    return True
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/", methods=["POST"])
+def upload():
+    if request.method == "POST":
+        if "file" in request.files:
+            file = request.files["file"]
+            if file and file_is_valid(file.filename):
+                filename = secure_filename(file.filename)
+                #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                file.save("tmp/"+filename)
+                flash("Upload successful")
+                return redirect(url_for("home"))
+        flash("Oh no...a file wasn't uploaded.")
+        return redirect(request.url)os.path.
