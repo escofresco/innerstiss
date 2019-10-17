89dc08aec6bc4df975d7e53994f18939b2d8e717
Jonasz Karwacki <jonaszakr@gmail.com>
2019-10-17 16:11:03 -0700

    complete route for transcript view and home

--- views.py
+++ views.py
@@ -1,14 +1,11 @@
 import os
 
-from flask import flash, redirect, render_template, request, url_for
+from botocore.exceptions import ClientError
+from flask import flash, redirect, render_template, request, session, url_for
 from werkzeug import secure_filename
 
 from app import app
-import helper
-
-
-def file_is_valid(filename):
-    return True
+import helpers
 
 
 @app.route("/")
@@ -21,25 +18,34 @@ def upload():
     if request.method == "POST":
         if "file" in request.files:
             file = request.files["file"]
-            if file and file_is_valid(file.filename):
+            if file and helpers.file_is_valid(file.filename):
                 filename = secure_filename(file.filename)
                 #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                file.save("tmp/" + filename)
+                filepath = "tmp/"+filename
+                file.save(filepath)
                 flash("Upload successful")
-                #return redirect(url_for("home"))
+                try:
+                    upload_res = helpers.upload_to_s3(filepath, filename)
+                except ClientError as e:
+                    return render_template("error_view.html", error_message=str(e))
+                try:
+                    transcribe_res = helpers.transcribe(filename)
+                except ClientError as e:
+                    return render_template("error_view.html", error_message=str(e))
+                transcript_uri = transcribe_res["TranscriptionJob"]["Transcript"]["TranscriptFileUri"]
+                session[filename] = helpers.load_json_from_uri(transcript_uri)
+                helpers.remove_from_s3(filename)
                 return redirect(
-                    url_for("view_transcription", transcription_id="someid"))
+                    url_for("view_transcript", transcript_id=filename))
         flash("Oh no...a file wasn't uploaded.")
         return redirect(request.url)
 
 
-@app.route("/<transcription_id>")
-def view_transcription(transcription_id):
+@app.route("/<transcript_id>")
+def view_transcript(transcript_id):
     try:
-        transcription = helper.transcription_by_id(transcription_id)
-        return render_template("transcription_view.html",
-                               title=transcription["title"],
-                               transcription_name=sample_title,
-                               transcription_content=sample_content)
-    except:
-        return render_template("error_view.html")
+        transcript = helpers.transcript_by_id(transcript_id)
+        return render_template("transcript_view.html",
+                               transcript_title=transcript["transcript_title"], transcript_content=transcript["transcript_content"])
+    except FileNotFoundError as e:
+        return render_template("error_view.html", error_message=str(e))
