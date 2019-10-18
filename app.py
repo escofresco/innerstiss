89c44c898e4fe1101f6241d93e418100a0f84d1f
Jonasz Karwacki <jonaszakr@gmail.com>
2019-10-18 12:43:23 -0700

    use secret key

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import os
+
 from flask import Flask
+from flask_session import Session
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
+
 app.config.from_object("config.Configuration")
-socketio = SocketIO(app)
+app.secret_key = os.getenv("SECRET_KEY")
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+
+socketio = SocketIO(app, manage_session=False)
